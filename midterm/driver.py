d0caa04070d147de10bbfe8eed3dcc9d3d94349d
Danny Nguyen <DannyNguyent1996@gmail.com>
2017-03-19 22:37:53 -0500

    Added problem 2 soluation
    Added more methods to calculate the best matching word with a sequence inside a text file. 
    Added a class to hold the PWM and a word matrix.

--- midterm/driver.py
+++ midterm/driver.py
@@ -1,5 +1,21 @@
 # Midterm Project for Bioinformatics - Danny Nguyen
 import sys
+import math
+
+# Class containing the PWM and the word matrix.
+class info():
+    def __init__(self):
+        self.PWM = create_PWM()
+        self.stringMatrix = wordMatrix()
+
+# Store the information of all the different word combinations.
+def wordMatrix():
+    words = []
+    with open("input1.txt") as file:
+        for line in file:
+            words.append(line)
+    return words
+
 # Zero's the matrix and returns a matrix.
 def zero(matrix):
     returnVal = []
@@ -18,7 +34,7 @@ def file_len(fName):
 
 # Creating PWM - Problem 1
 def create_PWM():
-    m = 4                       # Number of rows (A, T, C, G)
+    m = 7                       # Number of rows (A, T, C, G, Highest P, H, I)
     n = file_len("input1.txt")  # Number of words to check
     PWM = zero((m+1, n+1))      # Create PWM matrix
     colCount = 0
@@ -48,22 +64,94 @@ def create_PWM():
             PWM[1][colCount] = p_C
             PWM[2][colCount] = p_G
             PWM[3][colCount] = p_T
-            
+
+            # Adding information content into the matrix.
+            PWM[4][colCount] = calculateH(PWM, colCount) 
+            PWM[5][colCount] = 2 - PWM[4][colCount]      
+                
             colCount += 1   # Increase the count after processing each word
     formatPrint(PWM) # After finish calculating data, print out the PWM
+    return PWM
+
+# Calculate the H for the word column.
+def calculateH(PWM, colCount):
+    H = 0
+    P_A, P_C, P_G, P_T = PWM[0][colCount], PWM[1][colCount], PWM[2][colCount], PWM[3][colCount]
+    
+    # Calculating the entropy of each character.
+    H_A = (-P_A * math.log(P_A, 2))
+    H_C = (-P_C * math.log(P_C, 2))
+    H_G = (-P_G * math.log(P_G, 2))
+    H_T = (-P_T * math.log(P_T, 2))
+
+    # Getting summation of all the entropies and returning the value
+    H = H_A + H_C + H_G + H_T
+    return H
+
+# Calculates the best match with a PWM against a sequence.
+def calculateBestMatch(PWM, wordlist, sequence, seqLen):
+    score = (float) (0)                     # Best score.
+    n = len(PWM)                            # How many different word to check with.
+    subString = [0, 0]                      # Position of best match.
+    word = ''                               # The word that best matched the sequence.
+    repeat = len(sequence) - seqLen + 1     # Number of times to repeat the search for the sequence.
+
+    # For each word in the PWM
+    for i in range(0, n):
+        start = 0       # Starting position
+        end = seqLen    # Ending position
+
+        # Probability of each A, C, G, T
+        p_A, p_C, p_G, p_T = PWM[0][i], PWM[1][i], PWM[2][i], PWM[3][i]
+        
+        # Number of repeats for the sequence
+        for temp in range(0, repeat):
+            matchScore = (float) (1)                # The match score for the individual sequence.
+
+            # For each word in the sequence
+            for j in range(start, end):
+                if (sequence[j] == 'A'):
+                    matchScore *= (float) (p_A)
+                if (sequence[j] == 'C'):
+                    matchScore *= (float) (p_C)
+                if (sequence[j] == 'G'):
+                    matchScore *= (float) (p_G)
+                if (sequence[j] == 'T'):
+                    matchScore *= (float) (p_T)
+
+            # If the match score is greater than the current max score
+            if (matchScore > score):  
+                score = matchScore              # Update all the information for current max  
+                subString[0] = start            # ^
+                subString[1] = end              # ^
+                word = wordlist[i]              # ^
+
+            # Update position checking
+            start += 1
+            end += 1
+
+    # Print out information for best match.        
+    print('Best Match: %sPosition: %d - %d\nScore: %s\n' % (word, subString[0], subString[1], score))
 
 # Format the printing of the PWM
 def formatPrint(PWM):
-    chars = ['A', 'C', 'G', 'T']
-    for x in range(0, 4):
+    chars = ['A', 'C', 'G', 'T', 'H', 'I']
+    for x in range(0, 6):
         sys.stdout.write("%c " %chars[x])
         for y in range(0, file_len("input1.txt")):
-            sys.stdout.write("%.2f " %PWM[x][y])
+            sys.stdout.write("%.2f " % PWM[x][y])
         sys.stdout.write("\n")
 
-
+# Main function to run the program.
 def main():
-    create_PWM()
+    information = info()
+    with open("input2.txt") as file:
+        i = 1
+        for line in file:
+            print("Sequence Number: %d" % i)
+            calculateBestMatch(information.PWM, information.stringMatrix, line, 7)
+            i += 1
 
+# Initialziation
 if __name__ == '__main__':
     main()
